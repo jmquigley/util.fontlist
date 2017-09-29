97e68daf5da83854ce1c885609f727ae9cb2f6ab
James Quigley <jmquigley@outlook.com>
2017-09-29 12:44:59 -0500

    Initial creation of the module

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # util.fontlist
-Detects the fonts that are available to the browser on the current system
+
+> Detects the fonts that are available to the browser on the current system
+
+[![Build Status](https://travis-ci.org/jmquigley/util.fontlist.svg?branch=master)](https://travis-ci.org/jmquigley/util.fontlist)
+[![tslint code style](https://img.shields.io/badge/code_style-TSlint-5ed9c7.svg)](https://palantir.github.io/tslint/)
+[![NPM](https://img.shields.io/npm/v/util.fontlist.svg)](https://www.npmjs.com/package/util.fontlist)
+
+
+## Installation
+
+This module uses [yarn](https://yarnpkg.com/en/) to manage dependencies and run scripts for development.
+
+To install as an application dependency:
+```
+$ yarn add --dev util.fontlist
+```
+
+To build the app and run all tests:
+```
+$ yarn run all
+```
+
+
+## Overview
+
+The `Detector` class can be used to determine if the browser supports the requested font.  It's based on the [algorithm](http://www.lalit.org/lab/javascript-css-font-detect/) created by Lalit Patel.  When the class is instantiated it takes three base fonts (monospace, sans-serif, and serif) and applies them to a base string.  The width and height of this rendered string, for each base, is saved.  The caller then uses the `detect` method to request detection of a font by name.  The base string is then rendered with the font name of the requested font.  The rendered output width and height are compared to the computed base versions from the previous step.  If they differ, then we assume that the browser can render that font.  If they are the same we assume the browser cannot.
+
+Note that this is meant to be used within a browser environment; it needs the DOM and rendering to work correctly.
+
+
+## Usage
+
+#### Basic Class Usage
+```javascript
+import {Detector} from 'util.fontlist';
+
+let detector = new Detector();
+if (detector.detect('Consolas')) {
+	console.log('Consolas is supported');
+} else {
+	console.log('Consolas is NOT supported');
+}
+```
+
+#### Conveniece Retrieval
+A method is provided with the module named `getFontList` that will take a list of font names, internally create the detector, check if each given font exists, and return the list of available fonts.
+
+```javascript
+import {getFontList} from 'util.fontlist';
+
+let fontList = getFontList();
+
+// ['Cambria', 'Consolas', ...]
+
+```
+
+By default this function will use the included file `fontlist.json` to get a list of possible fonts.  This can be overridden in the call to `getFontList` to use a custom list:
+
+```javascript
+import {getFontList} from 'util.fontlist';
+
+let fontList = getFontList(['Consolas', 'Cambria']);
+
+// ['Cambria', 'Consolas', ...]
+```
+
+This will only search for the two fonts given as a parameter instead of the default list.
---
+++ index.ts
@@ -0,0 +1,135 @@
+'use strict';
+
+const debug = require('debug')('fontlist');
+
+interface FontSize {
+	width: number;
+	height: number;
+}
+
+interface Fonts {
+	[key: string]: FontSize;
+}
+
+export enum FontType {
+	all,
+	fixed,
+	variable
+}
+
+export const fonts: any = require('./fontlist.json');
+export const fontVariable: string[] = fonts['variable'];
+export const fontFixed: string[] = fonts['fixed'];
+
+export class Detector {
+
+	private baseFonts: Fonts = {
+		'monospace': {
+			width: 0,
+			height: 0
+		},
+		'sans-serif': {
+			width: 0,
+			height: 0
+		},
+		'serif': {
+			width: 0,
+			height: 0
+		}
+	};
+
+	private body: any;
+	private span: any;
+
+	// Use m or w because these two characters take up the maximum width.
+	// And use LLi so that the same matching fonts can get separated
+	private readonly testString: string = 'mmmmmmmmmmlli';
+	private readonly testSize: string = '72px';
+
+	constructor() {
+		this.body = document.getElementsByTagName('body')[0];
+		this.span = document.createElement('span');
+
+		this.span.style.fontSize = this.testSize;
+		this.span.innerHTML = this.testString;
+
+		for (const key of Object.keys(this.baseFonts)) {
+			this.span.style.fontFamily = key;
+
+			this.body.insertAdjacentElement('afterbegin', this.span);
+			this.baseFonts[key].width = this.span.offsetWidth;
+			this.baseFonts[key].height = this.span.offsetHeight;
+			this.body.removeChild(this.span);
+		}
+	}
+
+	/**
+	 * Adds the requested font to the document body.  It checks to see if the
+	 * width or height of the testString has changed when compared to the
+	 * computed baseFonts.  If there is a change, then detect returns
+	 * true.  If all baseFonts are checked and there is no change, then the
+	 * font is not available.
+	 * @param fontName {string} the name of the font to check in the browser
+	 * @return true if the font is available, otherwise false.
+	 */
+	public detect(fontName: string) {
+		for (const key of Object.keys(this.baseFonts)) {
+			this.span.style.fontFamily = `${fontName},${key}`;
+			this.body.insertAdjacentElement('afterbegin', this.span);
+
+			const matched: boolean = (this.span.offsetWidth !== this.baseFonts[key].width
+				|| this.span.offsetHeight !== this.baseFonts[key].height);
+			this.body.removeChild(this.span);
+			if (matched) {
+				debug('%s is available', fontName);
+				return true;
+			}
+		}
+
+		debug('%s is NOT available', fontName);
+		return false;
+	}
+}
+
+/**
+ * A convenience function that checks a base list of possible fonts and returns
+ * the list that was found.  It uses the file "fontlist.json" to list the fonts
+ * that will be used by default.  Expand this list to expand default lookup coverage.
+ * The caller can also pass a list of fonts and override the default "fontlist.json"
+ * @param fontList {string[]} a custom list of fonts that will be used for the
+ * search list.  If empty the "fontlist.json" will be used.
+ * @param fontType {FontType} two types of fonts: fixed and variable.  By default
+ * all types will be searched.  This allows the caller to segregate types if
+ * desired.
+ * @return {string[]} an array of strings containing valid fonts.
+ */
+export function getFontList(fontList: string[] = [], fontType: FontType = FontType.all): string[] {
+	const l: string[] = [];
+	const detector = new Detector();
+
+	if (fontList.length === 0) {
+		switch (fontType) {
+			case FontType.fixed:
+				fontList = fontFixed;
+				break;
+
+			case FontType.variable:
+				fontList = fontVariable;
+				break;
+
+			case FontType.all:
+			default:
+				fontList = fontList.concat(fontFixed, fontVariable);
+				break;
+		}
+	}
+
+	debug('list of possible fonts: %O', fontList);
+	for (const fontName of fontList) {
+		if (detector.detect(fontName)) {
+			l.push(fontName);
+		}
+	}
+
+	return l;
+}
